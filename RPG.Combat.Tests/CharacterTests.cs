460605abb1fc26fe777a002d69da8da05e71813c
Hamervit <harold.mercado@sinco.com.co>
2025-04-25 12:43:16 -0500

    🔴 Si_UnPersonajeEsCreadoSinTipo_Debe_ArrojarUnArgumentNullException

--- RPG.Combat.Tests/CharacterTests.cs
+++ RPG.Combat.Tests/CharacterTests.cs
@@ -1,6 +1,22 @@
+using FluentAssertions;
+
 namespace RPG.Combat.Tests;
 
 public class CharacterTests
 {
-    
+    [Fact]
+    public void Si_UnPersonajeEsCreadoSinTipo_Debe_ArrojarUnArgumentNullException()
+    {
+        var caller = () => new Personaje(null!);
+
+        caller.Should().ThrowExactly<ArgumentNullException>();
+    }
+}
+
+public class Personaje
+{
+    public Personaje(object o)
+    {
+        throw new NotImplementedException();
+    }
 }
